72297d847f319a06f9f88bdb1f78ab9e865371d7
ditole <tolerevo@hotmail.com>
2024-04-25 01:54:00 -0600

    add nav stylying

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { getPetDetails } from '../../api/petfinder';
-import Hero from '../../components/hero';
+import React, { useEffect, useState } from "react";
+import { getPetDetails } from "../../api/petfinder";
+import Hero from "../../components/hero";
+
+import { useParams } from "react-router-dom";
 
 // Import useParams
 // Import Navigate
@@ -9,7 +11,7 @@ const PetDetailsPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const id = '51322435'; // <--- Update me!
+  const { id } = useParams(); // <--- Update me!
 
   useEffect(() => {
     async function getPetsData() {
@@ -37,7 +39,7 @@ const PetDetailsPage = () => {
       ) : (
         <main>
           <Hero
-            image={data.photos[1]?.full || 'https://i.imgur.com/aEcJUFK.png'}
+            image={data.photos[1]?.full || "https://i.imgur.com/aEcJUFK.png"}
             displayText={`Meet ${data.name}`}
           />
           <div className="pet-detail">
@@ -45,7 +47,7 @@ const PetDetailsPage = () => {
               <img
                 className="pet-image"
                 src={
-                  data.photos[0]?.medium || 'https://i.imgur.com/aEcJUFK.png'
+                  data.photos[0]?.medium || "https://i.imgur.com/aEcJUFK.png"
                 }
                 alt=""
               />
@@ -53,7 +55,7 @@ const PetDetailsPage = () => {
             <div>
               <h1>{data.name}</h1>
               <h3>Breed: {data.breeds.primary}</h3>
-              <p>Color: {data.colors.primary || 'Unknown'}</p>
+              <p>Color: {data.colors.primary || "Unknown"}</p>
               <p>Gender: {data.gender}</p>
               <h3>Description</h3>
               <p>{data.description}</p>
