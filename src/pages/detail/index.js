e4376995b9328508c198d9f17e4191b5270c84c2
ditole <tolerevo@hotmail.com>
2024-04-25 02:39:32 -0600

    add error page and redirect path

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getPetDetails } from "../../api/petfinder";
 import Hero from "../../components/hero";
 
-import { useParams } from "react-router-dom";
+import { useParams, Navigate} from "react-router-dom";
 
 // Import useParams
 // Import Navigate
@@ -34,7 +34,7 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Navigate to="/pet-details-not-found" /> 
         </div>
       ) : (
         <main>
--- src/pages/petDetailsNotFound/index.js
+++ src/pages/petDetailsNotFound/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 // import useNavigate here.
 
 const PetDetailsNotFound = () => {
-
   // get the navigate function from useNavigate
-  const navigate = "REPLACE ME";
+  const navigate = useNavigate();
 
   const goHome = () => {
-    // Go home!
-  }
-  
+    navigate("/"); // Go home!
+  };
+
   return (
     <main className="page">
       <h3>404: Who let the dogs out?</h3>
-      <p>Sorry, but the details for this pet have not been uploaded by the shelter yet. Check back later!</p>
+      <p>
+        Sorry, but the details for this pet have not been uploaded by the
+        shelter yet. Check back later!
+      </p>
       <img
         src="https://i.chzbgr.com/full/8362031616/h9EB970C5/weve-lost-our-corgination"
         alt=""
