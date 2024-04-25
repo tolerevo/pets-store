88e5356439ae5697206558cf49156c2eb1c63307
ditole <tolerevo@hotmail.com>
2024-04-25 01:16:06 -0600

    add root route, index and outlet element

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
+// Add react-router-dom imports
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+// create router with JSX Route elements
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={appRouter} />;
+}
+
+export default App;
